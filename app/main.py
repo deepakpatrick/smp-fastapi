0ac8c085afee9bb50e752216abb6f9bd34e3accd
Deepak Patrick <deepakpatrick@yahoo.co.in>
2022-01-31 02:02:50 -0700

    deploying to heroku thru CI/CD

--- app/main.py
+++ app/main.py
@@ -34,7 +34,7 @@ app.include_router(votes.router)
 # Home page or Root
 @app.get("/")
 async def root():
-    return {"message":"SMP Fastapi comprehensive - now syncs with docker container - using bind service"}
+    return {"message":"SMP Fastapi - deployed from CI/CD"}
 
      
      
